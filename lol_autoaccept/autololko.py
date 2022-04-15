53dc22c41bac7567ec2db71446c587739f3046a1
konasek20 <kubajzik20@gmail.com>
2022-04-15 19:37:26 +0200

    Add files via upload

---
+++ lol_autoaccept/autololko.py
@@ -0,0 +1,20 @@
+import time
+import json
+import tkinter as tk
+import pyautogui
+from python_imagesearch.imagesearch import imagesearch, imagesearch_loop
+
+
+pyautogui.FAILSAFE = False
+TIMELAPSE = 1
+
+def main() :
+ while True:
+    pos = imagesearch("./prijmout.png")
+    pos = imagesearch("./cz.png")
+    if pos[0] != -1: 
+     pyautogui.moveTo(pos[0], pos[1])
+     pyautogui.click(clicks=2)
+     print("position : ", pos[0], pos[1])
+     print("hra byla přijata")
+    
